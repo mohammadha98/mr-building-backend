6beb825c3b3fcc781d0fe36218b56392cd461a69
unknown <mohammadhg98@gmail.com>
2026-06-28 03:06:54 -0700

    docker yml update

--- dist/src/modules/v2/redis/redis.module.d.ts
+++ dist/src/modules/v2/redis/redis.module.d.ts
@@ -1,7 +1,2 @@
-import { OnModuleInit } from "@nestjs/common";
-import { Cache } from "cache-manager";
-export declare class RedisModule implements OnModuleInit {
-    private cache;
-    constructor(cache: Cache);
-    onModuleInit(): void;
+export declare class RedisModule {
 }
--- dist/src/modules/v2/redis/redis.module.js
+++ dist/src/modules/v2/redis/redis.module.js
@@ -5,48 +5,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RedisModule = void 0;
 const common_1 = require("@nestjs/common");
+const ioredis_1 = require("@nestjs-modules/ioredis");
 const redis_service_1 = require("./redis.service");
-const redis_controller_1 = require("./redis.controller");
-const redisStore = require("cache-manager-redis-store");
-const cache_manager_1 = require("@nestjs/cache-manager");
 let RedisModule = class RedisModule {
-    constructor(cache) {
-        this.cache = cache;
-    }
-    onModuleInit() {
-        const logger = new common_1.Logger("RedisModule");
-        logger.log("Redis cache initialized");
-    }
 };
 RedisModule = __decorate([
     (0, common_1.Module)({
         imports: [
-            cache_manager_1.CacheModule.registerAsync({
-                useFactory: () => ({
-                    store: redisStore,
-                    host: process.env.REDIS_HOST || "suitable-stallion-154730.upstash.io",
-                    port: parseInt(process.env.REDIS_PORT || "6379"),
-                    password: process.env.REDIS_PASSWORD,
-                    username: 'mohammad',
-                    tls: process.env.REDIS_TLS === "true" ? {} : undefined,
-                }),
+            ioredis_1.RedisModule.forRoot({
+                config: Object.assign(Object.assign({ host: process.env.REDIS_HOST || "redis", port: parseInt(process.env.REDIS_PORT, 10) || 6379, password: process.env.REDIS_PASSWORD || undefined }, (process.env.REDIS_TLS === "true" && {
+                    tls: {},
+                })), { lazyConnect: false, retryStrategy(times) {
+                        const delay = Math.min(times * 50, 2000);
+                        return delay;
+                    } }),
             }),
         ],
-        exports: [redis_service_1.RedisService],
-        controllers: [redis_controller_1.RedisController],
         providers: [redis_service_1.RedisService],
-    }),
-    __param(0, (0, common_1.Inject)(cache_manager_1.CACHE_MANAGER)),
-    __metadata("design:paramtypes", [Object])
+        exports: [redis_service_1.RedisService],
+    })
 ], RedisModule);
 exports.RedisModule = RedisModule;
 //# sourceMappingURL=redis.module.js.map
